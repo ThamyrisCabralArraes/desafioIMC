0270a412caca359a8a9b594b56158ad651ab3a2d
Thamyris <thamyriscabral@hotmail.com>
2022-11-03 09:32:40 -0300

    meio

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 function classificacaoIMC (peso, altura) {
   let resultado = peso / (altura ** 2)
-  if(resultado < 18) {
-    return 'abaixo do peso normal'
+  if(resultado < 18.9) {
+    return 'Abaixo do peso normal'
   }
-  else if(resultado > 18 && resultado < 24) {
-    return 'peso normal'
+  else if(resultado < 24.9) {
+    return 'Peso normal'
   }
-  else if(resultado > 25 && resultado < 29) {
-    return 'excesso de peso'
+  else if(resultado < 29.9) {
+    return 'Excesso de peso'
   }
-  else if(resultado > 30 && resultado < 34) {
-    return 'obesidade grau I'
+  else if(resultado < 34.9) {
+    return 'Obesidade grau I'
   }
   else {
     return 'Obesidade'
   }
 }
-console.log(classificacaoIMC(40, 1,65))
+console.log(classificacaoIMC(70, 1,65))
